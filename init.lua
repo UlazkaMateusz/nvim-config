3edfd163f0f35cd0ad2ed18cc8bb3778c394c8c0
Ulążka Mateusz <ulazkamateusz@gmail.com>
2023-08-29 22:44:50 +0200

    Add plugins dir, enable hlsearch

--- init.lua
+++ init.lua
@@ -227,7 +227,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -235,7 +235,7 @@ require('lazy').setup({
 -- NOTE: You can change these options as you wish!
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 
 -- Make line numbers default
 vim.wo.number = true
